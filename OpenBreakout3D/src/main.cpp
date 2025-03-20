06551495e9c8cafc1b9dd51f4a9385fbdce286ff
Christos Mark <baratheoniron@gmail.com>
2025-03-20 08:49:07 -0400

    Initial commit. All libraries link and project builds.

---
+++ OpenBreakout3D/src/main.cpp
@@ -0,0 +1,12 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <fmt/core.h>
+
+GLFWwindow* window;
+
+int main(int argc, char** argv)
+{
+    fmt::println("Greetings!");
+    return 0;
+}
